7e8896477bb5afc1492d8a02201b119405f4e80d
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2025-07-28 10:01:09 -0300

    feat: luxon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import { DateTime } from 'luxon'
+
+const now = new Date()
+console.log('🚀 ~ now:', now)
+
+const classStartsInDateTime =
+  DateTime.fromJSDate(now).setZone('America/Sao_Paulo')
+
+const a = classStartsInDateTime.minus({ day: 1 }).startOf('day')
+const b = classStartsInDateTime.plus({ day: 1 }).endOf('day')
+
+console.log(a)
+console.log(b)
+console.log(a.diff(b))
+console.log(a < b)
