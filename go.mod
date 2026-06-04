179a2b64b0ea5398eb89c9041a5465fc20eed573
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2026-06-04 11:32:31 -0300

    feat: implement password hashing and verification using golang.org/x/crypto/bcrypt

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tests
 
 go 1.26.3
+
+require golang.org/x/crypto v0.52.0
--- src/main.go
+++ src/main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println(math.Pow(2, 10))
+	hash, err := bcrypt.GenerateFromPassword([]byte("batata"), 10)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(hash))
+
+	compareResult := bcrypt.CompareHashAndPassword(hash, []byte("batata"))
+	isSamePassword := compareResult == nil
+
+	fmt.Println("comparing hash with batata, result =", isSamePassword)
+
+	compareResult = bcrypt.CompareHashAndPassword(hash, []byte("potato"))
+	isSamePassword = compareResult == nil
+
+	fmt.Println("comparing hash with potato, result =", isSamePassword)
 }
